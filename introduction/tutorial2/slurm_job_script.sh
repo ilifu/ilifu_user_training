c621a276b4a7bfe129abaf57ff06b73c8364f737
Oarabile-Hope <oarabile@idia.ac.za>
2022-03-29 00:48:22 +0200

    updates

--- introduction/tutorial2/slurm_job_script.sh
+++ introduction/tutorial2/slurm_job_script.sh
@@ -3,14 +3,13 @@
 #SBATCH --job-name=demo_job
 #SBATCH --time=00:00:10
 #SBATCH --mem=4GB
-#SBATCH --reservation=ilifu_training
 #SBATCH --partition=Main
-#SBATCH --output=demo-job-%j.out
-#SBATCH --error=demo-job-%j.err
+#SBATCH --output=tic-tac_output/demo-job-%j.out
+#SBATCH --error=tic-tac_output/demo-job-%j.err
 #SBATCH --mail-user=oarabile@idia.ac.za
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
 #SBATCH --account=b34-admins-ag
 
 echo "------   Running demo job : Lets play TIC-TAC -----"
 
-singularity exec /idia/software/containers/python3/python3-2020-01-28.simg python job_script.py
+singularity exec /idia/software/containers/python-3.6.img python3 job_script.py 

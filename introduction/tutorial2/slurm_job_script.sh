5a25fcce02d847343fa76dfe2cbba38940e5928a
Oarabile-Hope <oarabile@idia.ac.za>
2022-04-02 13:22:57 +0200

    Update removing credentials

--- introduction/tutorial2/slurm_job_script.sh
+++ introduction/tutorial2/slurm_job_script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=demo_job
-#SBATCH --time=00:00:10
-#SBATCH --mem=4GB
+#SBATCH --time=00:00:25
+#SBATCH --mem=4G
 #SBATCH --partition=Main
 #SBATCH --output=tic-tac_output/demo-job-%j.out
 #SBATCH --error=tic-tac_output/demo-job-%j.err
-#SBATCH --mail-user=oarabile@idia.ac.za
+#SBATCH --mail-user=<user-email>
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --account=b34-admins-ag
+#SBATCH --account=<user-accounting-group>
 
 echo "------   Running demo job : Lets play TIC-TAC -----"
 

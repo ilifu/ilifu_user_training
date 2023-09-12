63e51741a62d552fc3ee12393af6972ba8105a83
Dane Kennedy <kennedy.dane@gmail.com>
2023-09-12 09:30:38 +0200

    show more info

--- introduction/tutorial2/hello_world.R
+++ introduction/tutorial2/hello_world.R
@@ -2,7 +2,19 @@ who_am_i <- function() {
   Sys.getenv("USER")
 }
 
+get_hostname <- function() {
+  Sys.getenv("HOSTNAME")
+}
+
+get_slurmjob_information <- function() {
+  all_info ← Sys.getenv()
+  all_info[grep("SLURM_JOB", names(all_info))]
+}
+
 
 i_am <- who_am_i()
+the_host <- get_hostname()
 
 print(paste("Hello", i_am))
+print(paste("I am running on", the_host)
+print(paste("Slurm Job environment variables:",get_slurmjob_information()))

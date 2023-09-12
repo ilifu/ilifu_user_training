0a833d8eecf4d99c40e9d47635aee683d60bb7f2
Dane Kennedy <kennedy.dane@gmail.com>
2023-09-12 07:16:30 +0200

    More informative python script

--- introduction/tutorial2/hello_world.py
+++ introduction/tutorial2/hello_world.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 
-def main():
-    print("Hello World!")
+import os
 
-if __name__ == "__main__":
+
+def who_am_i():
+    return os.getlogin()
+
+
+def get_hostname():
+    return os.uname().nodename
+
+
+def get_slurm_job_information():
+    return [env_var for env_var in os.environ if env_var.startswith('SLURM_JOB')]
+
+
+def main():
+    print(f'Hello {who_am_i()}!')
+    print(f'This snippet of code is running on "{get_hostname()}"')
+    print()
+    print('The following SLURM environment variables are set:')
+    print('\n'.join(get_slurm_job_information()))
+    
+
+if __name__ == '__main__':
     main()

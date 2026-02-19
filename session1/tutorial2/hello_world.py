96a400cbe2c9a677ad58cbe69f2f2b74ebc39f69
Dane Kennedy <kennedy.dane@gmail.com>
2026-02-19 15:50:05 +0200

    Make it a bit shorter

--- session1/tutorial2/hello_world.py
+++ session1/tutorial2/hello_world.py
@@ -2,29 +2,21 @@
 import os
 from sys import stderr
 
-def who_am_i():
-    return os.environ['USER']
-
-
-def get_hostname():
-    return os.uname().nodename
-
-
 def get_slurm_job_information():
     keys = [env_var for env_var in os.environ if env_var.startswith('SLURM_JOB')]
     return [f'{key}={os.environ[key]}' for key in keys]
 
 
 def main():
-    the_host = get_hostname()
-    the_user = who_am_i()
+    the_host = os.uname().nodename
+    the_user = os.environ['USER']
     print(f'Hello {the_user}!')
-    print(f'This snippet of code is running on "{the_host}"')
-    print()
+    print(f'This snippet of code is running on "{the_host}"\n')
+
     slurm_information = get_slurm_job_information()
     if slurm_information:
         print('The following SLURM environment variables are set:')
-        print('\n'.join(get_slurm_job_information()))
+        print('\n'.join(slurm_information))
     else:
         print(f'As far as I can tell, this is not a slurm job.')
 
